df812a276824a42b81b82df2b9f567e182239aac
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2020-05-13 21:03:14 +0530

    Add files via upload

---
+++ Remove all duplicates from a given string in Python.py
@@ -0,0 +1,10 @@
+def remove_duplicates(value):
+    a = " "
+    for i in value:
+        if i in a:
+            pass
+        else:
+            a = a + i
+    print(a)
+remove_duplicates("ParthPatel")
+
