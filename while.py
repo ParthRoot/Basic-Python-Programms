059e6a4fb51a8a0e1b68c2ab7e375ffffc9db1d6
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2021-03-16 22:51:18 +0530

    Add files via upload

--- while.py
+++ while.py
@@ -1,18 +1,11 @@
-i=0
-while i <= 4:
-    j = 0
-    while j <= 4:
-        print("X",end="")
-        j = j + 1
+i = 1
+while i<=5:
+    print("*", end=" ")
+    j = 1
+    while j<=5:
+        print("*", end=" ")
+        j=j+1
+
     i=i+1
     print()
-
-i = 1
-sum = 0
-while i <= 3:
-    sum = sum + i
-    i = i + 1
-print(sum)
-    
-    
 
